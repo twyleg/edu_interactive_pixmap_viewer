1a3b08cc66b67b16f56ac48e0fdd1b551c08dd62
Torsten Wylegala <mail@twyleg.de>
2025-10-26 23:13:47 +0100

    Changed left over from template

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change "interactive-pixmap-viewer" to your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/playground_react_vite_github_pages/',
+  base: '/edu_interactive_pixmap_viewer/',
 })
